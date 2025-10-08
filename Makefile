a82dc56885332ccf111f77cf88e889c8ed8a2086
Sasha <aosipenko@mozilla.com>
2025-10-08 16:29:29 +0200

    apply pre-commit hook for all files

--- Makefile
+++ Makefile
@@ -2,20 +2,19 @@ PYTHON=python3
 VENV=.venv
 REQS=requirements.txt
 
-.PHONY: all setup run clean qdrant-up 
+.PHONY: all setup run clean qdrant-up
 
 all: setup
 
 setup:
 	$(PYTHON) -m venv $(VENV)
-	$(VENV)/bin/pip install --upgrade pip 
+	$(VENV)/bin/pip install --upgrade pip
 	$(VENV)/bin/pip install -r $(REQS)
 	$(VENV)/bin/pip install -e .
-  
+
 litellm-proxy:
 	$(VENV)/bin/litellm-proxy
 
 
 clean:
 	rm -rf __pycache__ .cache $(VENV)
-
--- alembic/versions/8c787d0fbdc8_create_challenges_table.py
+++ alembic/versions/8c787d0fbdc8_create_challenges_table.py
@@ -1,25 +1,25 @@
 """Create challenges table
 
 Revision ID: 8c787d0fbdc8
-Revises: 
+Revises:
 Create Date: 2025-09-11 09:57:13.253483
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '8c787d0fbdc8'
+revision: str = "8c787d0fbdc8"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.execute("""
+	op.execute("""
         CREATE TABLE challenges (
             key_id VARCHAR(255) PRIMARY KEY,
             challenge VARCHAR(255),
@@ -29,4 +29,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.drop_table("challenges")
+	op.drop_table("challenges")
